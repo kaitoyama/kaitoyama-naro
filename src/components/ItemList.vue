d116787cc4d5dca8431f86b5bc29363e279c28a7
kaitoyama <uitakato@gmail.com>
2022-05-18 20:59:10 +0900

    買い物メモは発展含め終わった

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -4,6 +4,16 @@ const items = ref([
   { name: "たまご", price: 100 },
   { name: "りんご", price: 160 },
 ]);
+const newItemName = ref("");
+const newItemPrice = ref(0);
+
+const addItem = () => {
+  if (newItemName.value != "") {
+    items.value.push({ name: newItemName.value, price: newItemPrice.value });
+    newItemName.value = "";
+    newItemPrice.value = 0;
+  }
+};
 </script>
 
 <template>
@@ -13,6 +23,17 @@ const items = ref([
       <div class="name">名前:{{ item.name }}</div>
       <div class="price">{{ item.price }}円</div>
     </div>
+  </div>
+  <div>
+    <label>
+      名前
+      <input v-model="newItemName" type="text" />
+    </label>
+    <label>
+      価格
+      <input v-model="newItemPrice" type="number" />
+    </label>
+    <button @click="addItem">add</button>
   </div>
 </template>
 
