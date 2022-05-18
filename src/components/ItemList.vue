1ecc95a9f0a2adb0e64975a5290edec12f74c3d0
kaitoyama <uitakato@gmail.com>
2022-05-18 21:02:10 +0900

    商品リスト完成

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -19,9 +19,10 @@ const addItem = () => {
 <template>
   <div>ItemList</div>
   <div v-for="item in items" :key="item.name">
-    <div class="item">
+    <div class="item" :class="{ over500: item.price >= 500 }">
       <div class="name">名前:{{ item.name }}</div>
       <div class="price">{{ item.price }}円</div>
+      <div v-if="item.price >= 10000">高額商品</div>
     </div>
   </div>
   <div>
@@ -37,4 +38,8 @@ const addItem = () => {
   </div>
 </template>
 
-<style></style>
+<style>
+.over500 {
+  color: red;
+}
+</style>
